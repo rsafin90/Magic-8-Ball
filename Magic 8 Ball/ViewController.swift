087800cf9e832b3dce26881cc0885b0afbeb86b3
rsafin90 <rsafin90@gmail.com>
2022-05-08 20:09:05 +0300

    - Ball Image array config - Randomizer config - Storyboard configs - Assets config - Some fixies

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -2,18 +2,18 @@
 //  ViewController.swift
 //  Magic 8 Ball
 //
-//  Created by Руслан Сафин on 08.05.2022.
+//  Created by Ruslan Safin on 08.05.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3")]
+        
+        imageView.image = ballArray.randomElement()
     }
-
-
 }
-
